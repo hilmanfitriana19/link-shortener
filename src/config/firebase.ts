38a4a63a3b79322633728609675927625672381b
hilmanfitriana19 <48647915+hilmanfitriana19@users.noreply.github.com>
2025-06-18 21:48:15 +0700

    Fallback to redirect for Google sign-in

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -15,5 +15,6 @@ const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 export const googleProvider = new GoogleAuthProvider();
+googleProvider.setCustomParameters({ prompt: 'select_account' });
 
 export default app;
