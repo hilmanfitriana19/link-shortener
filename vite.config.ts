2b81973f301a67bd3a7af1e4d6b68630c65ce4eb
Hilman Fitriana <hilmanfitriana39@gmail.com>
2025-06-19 22:48:17 +0700

    chore: add handling 404 page

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,19 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base:'/link-shortener/',
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
-});
+  base: '/link-shortener/',
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'dist/index.html',
+          dest: 'dist',
+          rename: '404.html'
+        }
+      ]
+    })
+  ]
+})
