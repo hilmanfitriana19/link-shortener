8a4309446007ccc05f67353998c5e3fb4ae592b6
Hilman Fitriana <hilmanfitriana39@gmail.com>
2025-06-19 22:03:43 +0700

    chore: deploy github pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base:'/link-shortener/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
